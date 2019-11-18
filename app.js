242fdbaefe178a4d3e32ec165be11f830d6ad5bd
hmt <1750074417@qq.com>
2019-11-18 19:25:13 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/*
+ * This file is generated and updated by Sencha Cmd. You can edit this file as
+ * needed for your application, but these edits will have to be merged by
+ * Sencha Cmd when upgrading.
+ */
+Ext.application({
+    name: 'cosmoTheme',
+
+    extend: 'cosmoTheme.Application',
+
+    requires: [
+        'cosmoTheme.view.main.Main'
+    ],
+
+    // The name of the initial view to create. With the classic toolkit this class
+    // will gain a "viewport" plugin if it does not extend Ext.Viewport. With the
+    // modern toolkit, the main view will be added to the Viewport.
+    //
+    mainView: 'cosmoTheme.view.main.Main'
+
+    //-------------------------------------------------------------------------
+    // Most customizations should be made to cosmoTheme.Application. If you need to
+    // customize this file, doing so below this section reduces the likelihood
+    // of merge conflicts when upgrading to new versions of Sencha Cmd.
+    //-------------------------------------------------------------------------
+});
